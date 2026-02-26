9e65dc8d28d761bbcfb315af57c272126426fef0
Fulori4 <mathe.lorant@student.ms.sapientia.ro>
2026-02-26 09:06:32 +0200

    Update lab_1_1.hs

--- 01.labor/lab_1_1.hs
+++ 01.labor/lab_1_1.hs
@@ -28,8 +28,66 @@ elojel1 n
     | n > 0 = "pozitiv"
     | otherwise = "nulla"
 
-max_ :: Int -> Int-> String
-max_ a b 
-    |   a>b  =a
-    |   a<b  =b
-    |   otherwise = "egyenlo"
+-- masodfoku egyenlet
+-- masodf::(Floating b, Ord b) => b -> b -> b -> (b, b)
+-- masodf a b c if delta < 0 then error "komplex szam" else (gy1,gy2)
+-- where 
+--     delta = b**2 - 4*a*c
+--     gy1 = (-b + sqrt delta)/(2*a)
+--     gy2=(-b - sqrt delta)/(2*a)
+
+
+masodF2::(Ord a,Floating a) => a -> a->a->[a]
+masodF2 a b c
+    |delta<0 = error "komplex szamok"
+    |delta==0 = [gy1]
+    |otherwise = [gy1, gy2]
+    where
+        delta = b**2 - 4*a*c
+        gy1 = (-b + sqrt delta)/(2*a)
+        gy2 =(-b - sqrt delta)/(2*a)
+
+elempar::Eq a =>(a,a) -> (a,a)->Bool
+elempar ep1 ep2 = if(a==d && b==c) || (a==c && b==d)then True else False
+    where
+        (a,b)=ep1
+        (c,d)=ep2
+
+fakt2::(Ord t,Num t)=>t -> t
+fakt2 n 
+    |n<0 = error "neg. szam"
+    |n==0 = 1
+    |otherwise = n* fakt2(n-1)
+
+hatvany::(Ord a,Floating a)=>a->a->a
+hatvany x n
+    |n<0 = error"neg. kitevo"
+    |otherwise = x ** n
+
+negyzetgyok::(Enum a,Floating a)=>a->[a]
+negyzetgyok n = [sqrt i |i<-[1..n]]
+
+negyzetszam::(Num a,Enum a)=>a->[a]
+negyzetszam n = [i ^ 2 | i<-[1..n]]
+
+nemNegyzet::(Enum a,Eq a,Floating a)=>a->[a]
+nemNegyzet n = [i|i<-[1..n],i /= (sqrt i ** 2)]
+
+parosOsztok x =[i | i<-[1..x],mod x i == 0,mod i 2 ==0]
+
+osztok x = [i | i <- [1..x], mod x i == 0]
+
+primszam x = osztok x == [1, x]
+
+primszamN n = [i | i <- [1..n], primszam i]
+
+
+
+
+main :: IO()
+main = do 
+    -- putStrLn "Masodfoku egyenlet"
+    -- print(masodF2 1 4 1)
+    -- putStrLn("masodfoku egyenlet 2:" ++ show (masodF2 4 10 6))
+    putStrLn "hatvany"
+    print(hatvany 2 3)
