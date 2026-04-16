416a53164328e9d54cb1bd18897111a67b039d65
Fulori4 <mathe.lorant@student.ms.sapientia.ro>
2026-04-16 08:53:32 +0300

    Update parcialisgyakorlo.hs

--- 06.labor/parcialisgyakorlo.hs
+++ 06.labor/parcialisgyakorlo.hs
@@ -1,3 +1,7 @@
+import Data.List (sort)
+import Data.List (intercalate)
+
+
 -- 1. Egy [(String, Int)] típusú lista eleme egy városnevet és a megfelelő népesség
 -- értéket tárolja. Írjunk egy Haskell függvényt, amely meghatározza, azokat a
 -- városokat, amelyek népesség értéke egy adott n értéknél nagyobb. A kapott
@@ -17,4 +21,39 @@
 -- ertekkel rendelkezo varos.”
 
 fel1 = do 
-    
+    let ls = [("sepsiszentgyorgy", 54000), ("kolozsvár", 330000),
+                ("marosvasarhely", 130000), ("temesvar", 310000), ("arad", 160000),
+                ("gyergyoszentmiklos", 18000), ("nagyvarad",196000)]
+        n = 150000        
+        ls1 = filter (\(_, nepesseg) -> nepesseg>n) ls
+        varosok = sort (map fst ls1)
+    if null varosok
+        then putStrLn ("Nincs " ++ show n ++ " erteknel nagyobb nepesseg ertekkel rendelkezo varos.")
+        else do
+            putStrLn ("A(z) " ++ show n ++ " nepesseg erteknel nagyobbal rendelkezo varosok a kovetkezok:")
+            mapM_ (\varos -> putStrLn ("- " ++ varos)) varosok
+
+
+
+-- 2. Írjunk egy Haskell függvényt, amely meghatározza egy bemeneti egész
+-- számokat tartalmazó lista azon elemeit, amelyek nem tartalmazzák a 0
+-- számjegyet. Az eredmény számokat szóközzel elválasztva írjuk ki a
+-- képernyőre.
+-- Például:
+-- ● Bemenet: [17603, 4005, 3223, 816252, 70, 23561, 9018007, 807, 61, 300]
+-- ● Kimenet: A 0 szamjegyet nem tartalmazo szamok a kovetkezok: 3223 816252
+-- 23561 61
+-- ● Amennyiben nincsenek ilyen számok, a kimenet a következő: “Nincsenek
+-- olyan szamok, amelyek nem tartalmazzak a 0 szamjegyet.”
+
+fel2 = do
+    let ls = [17603, 4005, 3223, 816252, 70, 23561, 9018007, 807, 61, 300]
+        joSzamok = filter (notElem '0' . show) ls
+
+    if null joSzamok
+        then putStrLn "Nincsenek olyan szamok, amelyek nem tartalmazzak a 0 szamjegyet."
+        else putStrLn ("A 0 szamjegyet nem tartalmazo szamok a kovetkezok: " ++ unwords (map show joSzamok))
+
+
+
+main = fel2
